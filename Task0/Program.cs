d661e7b24162dd0f88288b0517fdf82e322cd660
Ivan <ivanazanov@gmail.com>
2022-07-17 18:04:26 +0500

    Добавил в метод 4 цикл for

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Тип 1
 
-void Method1() 
+void Method1()
 {
     Console.WriteLine("Просто выдает на печть");
 }
 
- Method1();
+Method1();
 
 // Тип 2
 
 void Method2(string msg, int count)
 {
-  int i = 0;
-while (i < count)
-{
-    Console.WriteLine(msg);
-    i++;
-}
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
 }
 
 Method2("Хасбик", 6);
@@ -29,28 +29,34 @@ int Method3()
 }
 
 int year = Method3();
-Console.WriteLine (year);
+Console.WriteLine(year);
 
 //Тип 4
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; //Пустая строка
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "ES");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-int i = 0;
-string result = String.Empty; //Пустая строка
-
-while (i < count)
-{
     
-result = result + text;
-    i++;
+    string result = String.Empty; //Пустая строка
+for (int i = 0; i < count; i++)
+{
+    result = result + text;
 }
-return result;
-
+    return result;
 }
-string res = Method4(10, "ES");
+string res = Method4(50, "ES");
 Console.WriteLine(res);
-
-
-
-
-
