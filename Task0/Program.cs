3a16e861f8cbc3cddc225145769695319af95d28
Ivan <ivanazanov@gmail.com>
2022-07-17 17:36:10 +0500

    Добавил метод 2

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -4,5 +4,19 @@ void Method1()
 {
     Console.WriteLine("Просто выдает на печть");
 }
-Method1();
+// Method1();
+
+// Тип 2
+
+void Method2(string msg, int count)
+{
+  int i = 0;
+while (i < count)
+{
+    Console.WriteLine(msg);
+    i++;
+}
+}
+
+Method2("Хасбик", 6);
 
