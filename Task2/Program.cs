965d4004e39badb8f2862daa6f765da1b94cac0b
Ivan <ivanazanov@gmail.com>
2022-07-17 18:49:41 +0500

    Задачад по замене символов в тексте

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Нужно заменить несколько символов в тексте
+// пробелы на черточки, маленькие к на  большие К, а большие С на маленькие с
+
+string text = "Деньги пахнут pussy, а Сумка Birkin, только нал Сумка Birkin, только нал";
+              
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+            
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '-');
+Console.WriteLine (newText);
+Console.WriteLine ();
+newText = Replace(newText, 'м', 'М');
+Console.WriteLine (newText);
+Console.WriteLine ();
+newText = Replace(newText, ',', '!');
+Console.WriteLine (newText);
